db43998001f05d7153a8141dc870307b09fba2d0
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-01-13 18:14:08 -0300

    Criando o UsuarioModel

---
+++ SlnSistemaContatos/src/Sistema.Cadastro.Contatos/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistema.Cadastro.Contatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+
+    }
+}
